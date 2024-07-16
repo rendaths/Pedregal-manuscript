e2d03193d3b2e8496d7f4035d9f058034ca80309
kirstyn <kirstyn@users.noreply.github.com>
2024-07-16 11:54:57 +0100

    Remove unused

--- scripts/changeFastaHeader.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-#changes fasta header to match filename
-for i in $(find . -name '*.fasta'); do
-new=$(basename $i)
-new=${new%.consensus.fasta}
-perl -i -pe "s/.*/>$new/ if $.==1" $i
-done
